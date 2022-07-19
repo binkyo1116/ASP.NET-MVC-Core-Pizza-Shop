26b124101bb00710bdb9adeea10f5f1e934aeb76
binkyo1116 <topdevprince@gmail.com>
2022-07-19 16:54:05 +0300

    Update HomeController.cs

--- OnlinePizzaWebApplication/OnlinePizzaWebApplication/Controllers/HomeController.cs
+++ OnlinePizzaWebApplication/OnlinePizzaWebApplication/Controllers/HomeController.cs
@@ -8,22 +8,21 @@ namespace OnlinePizzaWebApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            return View();
-        }
-
         public IActionResult Contact()
         {
             return View();
         }
 
         public IActionResult Error()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
